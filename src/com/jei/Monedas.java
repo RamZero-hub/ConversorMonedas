e8ddc4e4f59e0fa69e9fae1525db25e7b4513ffd
Jeisson <zeroplayjv@gmail.com>
2026-03-03 00:11:20 -0500

    Creacion del paquete com.jei

--- src/com/jei/Monedas.java
+++ src/com/jei/Monedas.java
@@ -1,5 +1,8 @@
+package com.jei;
+
 public record Monedas(String base_code,
                       String target_code,
                       double conversion_rate,
                       double conversion_result) {
+
 }
